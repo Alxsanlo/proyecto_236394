76a82de9f7eb1fbc655d1d54ac89af816f0ff178
Emilio Alejandro Sánchez López <Alexsanlo90@hotmail.com>
2021-05-24 19:10:44 -0500

    Limpieza codigo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const morgan = require('morgan');
 const express = require('express');
 const app = express();
 //Routesrs
-const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
 //Middelware
 const auth = require('./middleware/auth');
@@ -16,26 +15,11 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-//app.use(bodyParser.json());
-//pp.use(bodyParser.urlencoded({ extended: true}));
-
-/* 
-Verbos HTTP 
-
-GET 	- Obtener recursos.
-POST 	- Almacenar recursos.
-PATCH	- Modificar una parte de un recurso.
-PUT		- Modificar un recurso completo.
-DELETE	- Borrar un recurso.
-*/
-
 app.get("/", index);
 
 app.use("/user", user);
 
 app.use(auth);
-
-app.use("/pokemon", pokemon);
 
 app.use(notFound);
 
